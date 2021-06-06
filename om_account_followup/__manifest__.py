983c95633037a22004945e64ad89c7c54686a4aa
Juan Pablo Garza <juanp@juanpgarza.com>
2021-06-06 17:26:14 -0300

    [13.0][MIG] om_account_followup

--- om_account_followup/__manifest__.py
+++ om_account_followup/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Customer Follow Up Management',
-    'version': '14.0.2.0.0',
+    'version': '13.0.1.0.0',
     'category': 'Accounting',
     'description': """Customer FollowUp Management""",
     'summary': """Customer FollowUp Management""",
